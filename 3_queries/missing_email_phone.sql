5ac09e4d02885a39c61df109934645cf48aa5388
TJ-Blinn <tjblinn@hotmail.com>
2021-10-01 01:09:17 +0000

    moved all query files into 1_queries directory

--- 3_queries/missing_email_phone.sql
+++
@@ -1,8 +0,0 @@
-SELECT name, id, cohort_id
-FROM students
-WHERE email IS NULL
-OR phone IS NULL;
-
--- SELECT name, id, cohort_id
--- FROM students
--- WHERE TRIM(email, phone) IS NULL;
--- 4_queries/non-gmail_non-phone.sql
+++
@@ -1,4 +0,0 @@
-SELECT name, email, id, cohort_id
-FROM students
-WHERE email NOT LIKE '%gmail.com'
-OR phone IS NULL;
